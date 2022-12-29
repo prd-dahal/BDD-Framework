c4b5e232addd7d996bd4c1c51deb09a5ec978797
sayyed-akhtar <sayyed.akhtar@hazesoft.co>
2022-12-29 12:01:57 +0545

    Added multiple fill date command and made some changes on feature and common step

--- BDD/e2e/step_definitions/common/common.steps.js
+++ BDD/e2e/step_definitions/common/common.steps.js
@@ -1,134 +1,147 @@
-import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
-import { splitDateIntoDayMonthYear } from "../../step_definitions/utils/index.js";
+import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { splitDateIntoDayMonthYear } from '../../step_definitions/utils/index.js';
 import {
   commonInterceptors,
   insonInterceptors,
   mshieldInterceptors,
-} from "../../../support/interceptors";
-import { envVariables, viewports } from "../../../config";
+} from '../../../support/interceptors';
+import { envVariables, viewports } from '../../../config';
 
-Given("my previous records are deleted of {string}", (PHONE_NUMBER) => {
+Given('my previous records are deleted of {string}', (PHONE_NUMBER) => {
   cy.removeUser(PHONE_NUMBER);
 });
 
-Given("the test is done in {string}", (DEVICE) => {
+Given('the test is done in {string}', (DEVICE) => {
   const deviceFound = viewports.find((device) => device === DEVICE);
-  console.log("The device is found", deviceFound);
+  console.log('The device is found', deviceFound);
   if (!deviceFound) {
-    throw new Error("The device is not found");
+    throw new Error('The device is not found');
   } else {
     cy.viewport(DEVICE);
   }
 });
 
-Given("interceptors are loaded", () => {
+Given('interceptors are loaded', () => {
   commonInterceptors();
-  switch (envVariables["COMPANY"]) {
-    case "INSON":
+  switch (envVariables['COMPANY']) {
+    case 'INSON':
       insonInterceptors();
       break;
-    case "MSHIELD":
+    case 'MSHIELD':
       mshieldInterceptors();
       break;
   }
 });
 
-When("I visit url from config", () => {
-  cy.visit(envVariables["CYPRESS_TEST_URL"]);
+When('I visit url from config', () => {
+  cy.visit(envVariables['CYPRESS_TEST_URL']);
 });
 
-When("I visit url {string}", (url) => {
+When('I visit url {string}', (url) => {
   cy.visit(url);
 });
 
 Then(
-  "click on TAG {string} that contains TEXT {string}",
+  'click on TAG {string} that contains TEXT {string}',
   (TAG, HTMLCONTENT) => {
     cy.get(`${TAG}`).contains(`${HTMLCONTENT}`).click();
   }
 );
 
 Then(
-  "select TEXT {string} of SELECTORNAME {string} and SELECTORVALUE {string}",
+  'select TEXT {string} of SELECTORNAME {string} and SELECTORVALUE {string}',
   (TEXT, SELECTORNAME, SELECTORVALUE) => {
     cy.get(`[${SELECTORNAME}=${SELECTORVALUE}]`).select(TEXT);
   }
 );
 
 Then(
-  "click on TAG {string} SELECTORNAME {string} SELECTORVALUE {string}",
+  'click on TAG {string} SELECTORNAME {string} SELECTORVALUE {string}',
   (TAG, SELECTORNAME, SELECTORVALUE) => {
     cy.get(`${TAG}[${SELECTORNAME}=${SELECTORVALUE}]`).click();
   }
 );
 
 Then(
-  "click on SELECTORNAME {string} SELECTORVALUE {string}",
+  'click on SELECTORNAME {string} SELECTORVALUE {string}',
   (SELECTORNAME, SELECTORVALUE) => {
     cy.get(`[${SELECTORNAME}="${SELECTORVALUE}"]`).click({ force: true });
   }
 );
 
-Then("click on {string}", (SELECTORVALUE) => {
+Then('click on {string}', (SELECTORVALUE) => {
   cy.get(`[data-testid="${SELECTORVALUE}"]`).click({ force: true });
 });
 
-Then("click on {string} element of {string}", (POSITION, SELECTORVALUE) => {
+Then('click on {string} element of {string}', (POSITION, SELECTORVALUE) => {
   cy.get(`[data-testid="${SELECTORVALUE}"]`)
     .eq(Number(POSITION))
     .click({ force: true });
 });
 
 Then(
-  "type TEXT {string} on SELECTORNAME {string} SELECTORVALUE {string}",
+  'type TEXT {string} on SELECTORNAME {string} SELECTORVALUE {string}',
   (TEXT, SELECTORNAME, SELECTORVALUE) => {
     cy.get(`[${SELECTORNAME}="${SELECTORVALUE}"]`).type(TEXT);
   }
 );
-Then("type TEXT {string} on {string}", (TEXT, SELECTORVALUE) => {
-  cy.get(`[${"data-testid"}="${SELECTORVALUE}"]`).type(TEXT);
+Then('type TEXT {string} on {string}', (TEXT, SELECTORVALUE) => {
+  cy.get(`[${'data-testid'}="${SELECTORVALUE}"]`).type(TEXT);
 });
 
-Then("select date {string} on SELECTORNAME {string}", (date, selector) => {
+Then('input number of people who need cover to {string}', (people) => {
+  for (let i = 1; i < Number(people); i++) {
+    cy.get(`[data-testid=increaseBtn]`).click();
+  }
+});
+
+Then('input dates {string} on SELECTORNAME {string}', (dates, selector) => {
+  const splittedDate = dates
+    .split(',')
+    .map((date) => splitDateIntoDayMonthYear(date.trim()));
+  cy.fillMultipleDate(splittedDate, selector);
+});
+
+Then('select date {string} on SELECTORNAME {string}', (date, selector) => {
   const splittedDate = splitDateIntoDayMonthYear(date);
   cy.fillDate(splittedDate, selector);
 });
 
-Then("FillOTP PHONE_NUMBER {string}", (PHONE_NUMBER) => {
+Then('FillOTP PHONE_NUMBER {string}', (PHONE_NUMBER) => {
   cy.fillOtpAPI(PHONE_NUMBER);
 });
 
-Then("wait for {string} miliseconds", (seconds) => {
+Then('wait for {string} miliseconds', (seconds) => {
   cy.wait(Number(seconds));
 });
 
 Then(
-  "upload IMAGE {string} on SELECTORNAME {string} SELECTORVALUE {string}",
+  'upload IMAGE {string} on SELECTORNAME {string} SELECTORVALUE {string}',
   (IMAGE, SELECTORNAME, SELECTORVALUE) => {
     cy.get(`[${SELECTORNAME}="${SELECTORVALUE}"]`).selectFile(
-      `${envVariables["CYPRESS_IMAGE_PATH"]}${IMAGE}`
+      `${envVariables['CYPRESS_IMAGE_PATH']}${IMAGE}`
     );
   }
 );
 
-Then("upload IMAGE {string} on {string}", (IMAGE, SELECTORVALUE) => {
+Then('upload IMAGE {string} on {string}', (IMAGE, SELECTORVALUE) => {
   cy.get(`[data-testid="${SELECTORVALUE}"]`).selectFile(
-    `${envVariables["CYPRESS_IMAGE_PATH"]}${IMAGE}`
+    `${envVariables['CYPRESS_IMAGE_PATH']}${IMAGE}`
   );
 });
 
-Then("wait until image is uploaded", () => {
-  cy.wait("@uploadImage");
+Then('wait until image is uploaded', () => {
+  cy.wait('@uploadImage');
 });
 
-Then("wait until user is registered", () => {
-  cy.wait("@registeredUser");
+Then('wait until user is registered', () => {
+  cy.wait('@registeredUser');
 });
-Then("wait until subscribe", () => {
-  cy.wait("@subscribe");
+Then('wait until subscribe', () => {
+  cy.wait('@subscribe');
 });
 
-Then("payme fillOtp with {string}", (OTP) => {
-  cy.wait("@paymeSendOTP");
+Then('payme fillOtp with {string}', (OTP) => {
+  cy.wait('@paymeSendOTP');
   cy.fillOtpPayme(OTP);
 });
