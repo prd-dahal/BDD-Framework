2da2dc09b121f7f401f9607d2427ff48872f81dd
sayyed-akhtar <sayyed.akhtar@hazesoft.co>
2023-01-03 08:06:32 +0545

    made changes for mshield test

--- BDD/config.js
+++ BDD/config.js
@@ -1,8 +1,9 @@
 ////// CHANGE HERE FOR DIFFERENT ENVIRONMENT
-const COMPANY = "INSON"; // "INSON" or "MSHIELD"
-const TEST_URL = "https://onboard.v2.taksure.xyz/en/life-insurance/form";
-const DASHBOARD_URL = "https://dashboard.v2.taksure.xyz/api/v1/";
-const CMS_URL = "https://cms.v2.taksure.xyz/api/v1/";
+const COMPANY = 'MSHIELD'; // "INSON" or "MSHIELD"
+const TEST_URL =
+  'https://onboard.mshield-dev.taksure.xyz/en/life-insurance/form';
+const DASHBOARD_URL = 'https://dashboard.mshield-dev.taksure.xyz/api/v1/';
+const CMS_URL = 'https://cms.mshield-dev.taksure.xyz/api/v1/';
 //////
 
 //DO NOT EDIT THIS
@@ -11,31 +12,31 @@ export const envVariables = {
   CYPRESS_TEST_URL: TEST_URL,
   CYPRESS_DASHBOARD_URL: DASHBOARD_URL,
   CYPRESS_CMS_URL: CMS_URL,
-  CYPRESS_DASHBOARD_USERNAME: "taksure",
-  CYPRESS_DASHBOARD_PASSWORD: "taksure$$$123",
+  CYPRESS_DASHBOARD_USERNAME: 'taksure',
+  CYPRESS_DASHBOARD_PASSWORD: 'taksure$$$123',
   CYPRESS_IMAGE_PATH: `BDD/fixtures/images/${COMPANY}/`,
-  PAYME_CHECKOUT_URL: "https://checkout.test.paycom.uz/api",
+  PAYME_CHECKOUT_URL: 'https://checkout.test.paycom.uz/api',
 };
 //DO NOT EDIT THIS
 
 export const viewports = [
-  "ipad-2",
-  "ipad-mini",
-  "iphone-3",
-  "iphone-4",
-  "iphone-5",
-  "iphone-6",
-  "iphone-6+",
-  "iphone-7",
-  "iphone-8",
-  "iphone-x",
-  "iphone-xr",
-  "iphone-se2",
-  "macbook-11",
-  "macbook-13",
-  "macbook-15",
-  "macbook-16",
-  "samsung-note9",
-  "samsung-s10",
-  "samsung-s10+",
+  'ipad-2',
+  'ipad-mini',
+  'iphone-3',
+  'iphone-4',
+  'iphone-5',
+  'iphone-6',
+  'iphone-6+',
+  'iphone-7',
+  'iphone-8',
+  'iphone-x',
+  'iphone-xr',
+  'iphone-se2',
+  'macbook-11',
+  'macbook-13',
+  'macbook-15',
+  'macbook-16',
+  'samsung-note9',
+  'samsung-s10',
+  'samsung-s10+',
 ];
